4eab9007c3da4f8b4d5570e06e38590e112e427b
zsecq94 <zsecq94@gmail.com>
2026-02-13 11:13:26 +0900

    fix : 폰트 경로 수정

--- src/app/[locale]/fonts.ts
+++ src/app/[locale]/fonts.ts
@@ -5,32 +5,32 @@ export const generalSans = localFont({
   display: "swap",
   src: [
     {
-      path: "../../../public/fonts/GeneralSans-Extralight.woff2",
+      path: "./fonts/GeneralSans-Extralight.woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/GeneralSans-Light.woff2",
+      path: "./fonts/GeneralSans-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/GeneralSans-Regular.woff2",
+      path: "./fonts/GeneralSans-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/GeneralSans-Medium.woff2",
+      path: "./fonts/GeneralSans-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/GeneralSans-Semibold.woff2",
+      path: "./fonts/GeneralSans-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/GeneralSans-Bold.woff2",
+      path: "./fonts/GeneralSans-Bold.woff2",
       weight: "700",
       style: "normal",
     },
@@ -42,32 +42,32 @@ export const pretendard = localFont({
   display: "swap",
   src: [
     {
-      path: "../../../public/fonts/Pretendard-Extralight.woff2",
+      path: "./fonts/Pretendard-Extralight.woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/Pretendard-Light.woff2",
+      path: "./fonts/Pretendard-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/Pretendard-Regular.woff2",
+      path: "./fonts/Pretendard-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/Pretendard-Medium.woff2",
+      path: "./fonts/Pretendard-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/Pretendard-Semibold.woff2",
+      path: "./fonts/Pretendard-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/Pretendard-Bold.woff2",
+      path: "./fonts/Pretendard-Bold.woff2",
       weight: "700",
       style: "normal",
     },
